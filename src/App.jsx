2ab6824d331ef637de6cf99cd6e6f0e6437dcecf
Hung <longvanleu69@gmail.com>
2023-12-12 20:53:38 +0700

    init

---
+++ src/App.jsx
@@ -0,0 +1,145 @@
+import { useState } from "react";
+import LoginInPage from "./LogInPage";
+import SignUpPage from "./SignUpPage";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+function App() {
+  const [isLogin, setIsLogin] = useState(false);
+
+  const toggleForm = () => {
+    const container = document.getElementById("container");
+    setIsLogin(!isLogin);
+    isLogin != false
+      ? container.classList.remove("active")
+      : container.classList.add("active");
+  };
+
+  return (
+    <div className="container" id="container">
+      {isLogin ? (
+        <div className="form-container sign-up">
+          <form>
+            <h1>Create Account</h1>
+            <div className="social-icons">
+              <a href="#" className="icon">
+                <i className="fa-brands fa-google-plus-g"></i>
+              </a>
+              <a href="#" className="icon">
+                <i className="fa-brands fa-facebook-f"></i>
+              </a>
+              <a href="#" className="icon">
+                <i className="fa-brands fa-github"></i>
+              </a>
+              <a href="#" className="icon">
+                <i className="fa-brands fa-linkedin-in"></i>
+              </a>
+            </div>
+            <span>or use your email for registeration</span>
+            <input type="text" placeholder="Name" />
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <button onClick={toggleForm} id="login">
+              Sign Up
+            </button>
+          </form>
+        </div>
+      ) : (
+        <div className="form-container sign-in">
+          <form>
+            <h1>Sign In</h1>
+            <div className="social-icons">
+              <a href="#" className="icon">
+                <i className="fa-brands fa-google-plus-g"></i>
+              </a>
+              <a href="#" className="icon">
+                <i className="fa-brands fa-facebook-f"></i>
+              </a>
+              <a href="#" className="icon">
+                <i className="fa-brands fa-github"></i>
+              </a>
+              <a href="#" className="icon">
+                <i className="fa-brands fa-linkedin-in"></i>
+              </a>
+            </div>
+            <span>or use your email password</span>
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <a href="#">Forget Your Password?</a>
+            <button onClick={toggleForm} id="register">
+              Sign In
+            </button>
+          </form>
+        </div>
+      )}
+      <div className="toggle-container">
+        <div className="toggle">
+          <div
+            className={`toggle-panel ${
+              isLogin ? "toggle-left" : "toggle-right"
+            }`}
+          >
+            <h1>{isLogin ? "Welcome Back!" : "Hello, Friend!"}</h1>
+            <p>
+              {isLogin
+                ? "Enter your personal details to use all site features"
+                : "Register with your personal details to use all site features"}
+            </p>
+            <button onClick={toggleForm}>
+              {isLogin ? "Sign In" : "Sign Up"}
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+{
+  /* <div className="container" id="container">
+{isLogin ? <div className="form-container sign-in">
+    <form>
+    <h1>Sign In</h1>
+          <div className="social-icons">
+              <a href="#" className="icon"><i className="fa-brands fa-google-plus-g"></i></a>
+              <a href="#" className="icon"><i className="fa-brands fa-facebook-f"></i></a>
+              <a href="#" className="icon"><i className="fa-brands fa-github"></i></a>
+              <a href="#" className="icon"><i className="fa-brands fa-linkedin-in"></i></a>
+          </div>
+          <span>or use your email password</span>
+          <input type="email" placeholder="Email"/>
+          <input type="password" placeholder="Password"/>
+          <a href="#">Forget Your Password?</a>
+          <button onClick={toggleForm}>Sign In</button>
+    </form>
+  </div> : <div className="form-container sign-up">
+<form>
+<h1>Create Account</h1>
+          <div className="social-icons">
+              <a href="#" className="icon"><i className="fa-brands fa-google-plus-g"></i></a>
+              <a href="#" className="icon"><i className="fa-brands fa-facebook-f"></i></a>
+              <a href="#" className="icon"><i className="fa-brands fa-github"></i></a>
+              <a href="#" className="icon"><i className="fa-brands fa-linkedin-in"></i></a>
+          </div>
+          <span>or use your email for registeration</span>
+          <input type="text" placeholder="Name"/>
+          <input type="email" placeholder="Email"/>
+          <input type="password" placeholder="Password"/>
+          <button onClick={toggleForm}>Sign Up</button>
+</form>
+</div>}
+<div className="toggle-container">
+  <div className="toggle">
+    <div className={`toggle-panel ${isLogin ? 'toggle-left' : 'toggle-right'}`}>
+      <h1>{isLogin ? 'Welcome Back!' : 'Hello, Friend!'}</h1>
+      <p>{isLogin ? 'Enter your personal details to use all site features' : 'Register with your personal details to use all site features'}</p>
+      <button onClick={toggleForm} >
+        {isLogin ? 'Sign In' : 'Sign Up'}
+      </button>
+    </div>
+  </div>
+</div>
+</div> */
+}
